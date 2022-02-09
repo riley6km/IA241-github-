1e986a0982a4166f0a3d783770d90093be65ec79
riley6km <kmriley25@outlook.com>
2022-02-09 20:04:32 +0000

    added my lab3.py file.

--- lab3.py
+++ lab3.py
@@ -26,7 +26,7 @@ my_list = [ 'a',' 123', 123, 'b', 'B', 'False', False, 123, None, 'None' ]
 print(  len(  set(my_list) )  )
 
 #3.6
-print(  len( "This is my third python lab.".split() ) 
+print(  len( "This is my third python lab.".split() ) )
 
 #3.7
 
@@ -34,7 +34,7 @@ num_list = [12, 32, 43, 35]
 
 num_list.sort()
 
-print(num_list [0]))
+print(num_list[0])
 print(num_list[-1])
 
 #3.8
