8a2309645fb745562b1446c421af3c4c78808ca2
riley6km <kmriley25@outlook.com>
2022-03-09 20:09:19 +0000

    lab and lec 7

---
+++ lec7.py
@@ -0,0 +1,29 @@
+'''
+lec 7
+while loop
+'''
+
+i=5
+
+while i>=0 :
+    
+    try:
+        print(1/(i-3))
+        
+    except:
+        continue
+    
+    i = i -1
+    
+   # if i ==3:
+       # pass
+    
+    #print(i)
+    
+#try:
+    #print(1/0)
+    
+#except:
+    #print('error 1')
+    
+    
